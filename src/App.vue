90842e71be720bcaada6f72ddf090345ccf317f0
Peter Berkecz <berkecz.peter@gmail.com>
2020-01-30 20:28:26 +0100

    Project rename

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <Calculator msg="Átlagszámoló"/>
+    <Calculator msg="Átlag számoló"/>
   </div>
 </template>
 
